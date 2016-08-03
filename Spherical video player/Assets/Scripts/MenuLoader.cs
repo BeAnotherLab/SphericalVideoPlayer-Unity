c75f8eaad6ff8954290aca7e8006a0a4889b9c98
Marte Roel <marteroel@gmail.com>
2016-08-02 21:40:37 -0500

    Added Unity Project

---
+++ Spherical video player/Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLoader : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	void Update () {
+		if (Input.GetKeyDown ("m")){
+			Debug.Log ("you just pressed the correct key");
+			loadingImage.SetActive (true);
+			Application.LoadLevel(0);
+		}
+	}
+}
---
+++ Spherical video player/Assets/Scripts/VideoPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class VideoPlayer : MonoBehaviour {
+	
+	private AudioClip myAudioClip;
+	MovieTexture myMovieTexture;
+	AudioSource myMovieAudio;
+
+	private bool isPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		myMovieTexture = (MovieTexture)GetComponent<Renderer> ().material.mainTexture;
+		myMovieTexture.loop = false;
+
+		float frame = myMovieTexture.duration;
+		Debug.Log ("The duration of this video is: " + frame + " seconds");
+
+		myMovieAudio = (AudioSource)GetComponent<AudioSource>();
+		myMovieAudio.clip = myMovieTexture.audioClip;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("space")) {
+			
+			if (!isPlaying) {
+				myMovieTexture.Play ();
+				myMovieAudio.Play ();
+				isPlaying = true;
+			}
+
+			else if (isPlaying) {
+				myMovieTexture.Pause ();
+				myMovieAudio.Pause ();
+				isPlaying = false;
+			}
+		
+		}
+		if (Input.GetKeyDown ("return")) {
+			myMovieTexture.Stop ();
+			myMovieAudio.Stop ();
+			}
+		}
+}
